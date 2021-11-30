28d85f52281c3527feff6c2520b619219968ea3e
Areebanadeem <areebanadeem818@gmail.com>
2021-11-30 12:24:32 +0500

    first commit

---
+++ Final/Final/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Final
+{
+    public abstract class Employee
+    {
+        public int EmployeeID;
+        public string EmployeeName;
+        public string EmployeeType;
+        public double HourWorked;
+        public double NoOfUnitProduced;
+        public double BaseHours = 160;
+        public double hourlyPayRate= 2.00;
+        public double OverTimeRate= 1.00;
+        double unitPay;
+         public abstract void calcSalary();
+    }
+}
---
+++ Final/Final/SalariedEmployee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Final
+{
+    public class SalariedEmployee : Employee
+    {
+      
+        public override void calcSalary()
+        {
+            if (HourWorked >= BaseHours)
+            {
+                double basePay = 500;
+                double overTimeHour = HourWorked - BaseHours;
+                double overTimePay = hourlyPayRate * overTimeHour * OverTimeRate;
+                double unitPay;
+
+                if (NoOfUnitProduced > 0 || NoOfUnitProduced < 100)
+                {
+                    unitPay= 3.000 * NoOfUnitProduced;
+
+                }
+               else if (NoOfUnitProduced > 101 || NoOfUnitProduced < 200)
+                {
+                    unitPay = 3.250 * NoOfUnitProduced;
+
+                }
+                else if (NoOfUnitProduced > 201 || NoOfUnitProduced < 300)
+                {
+                    unitPay = 3.500 * NoOfUnitProduced;
+
+                }
+                else 
+                {
+                    unitPay = 4.000 * NoOfUnitProduced;
+
+                }
+                double GrossPay = basePay + overTimePay + unitPay;
+
+            }
+        }
+
+        
+
+
+        }
+
+}
